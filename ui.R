a484fb0df01ed9bcd8052999a8d1534fe819c629
nurulnabilahr237 <nurulnabilahr@gmail.com>
2017-06-05 11:28:56 +0800

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,51 +1,60 @@
-
-
-
 library(shiny)
 
+abc <- read.csv("ListOfFeesByUniversity.csv", header = TRUE, sep = ",")
+abc = abc[,apply(abc, 2, function(x){sum(!is.na(x))>0}),]
+abc = abc[!apply(is.na(abc) | abc == "",1,all),]
 
 shinyUI(fluidPage(
-  titlePanel("University Fees"),
+  titlePanel("University Finder"),
+  
   sidebarLayout(
     sidebarPanel(
       
+      selectInput("type",
+                  "Type of University :",
+                  c("All", 
+                  unique(as.character(abc$Type)))),
+                  
+      selectInput("state", 
+                  "State :",
+                  c("All",
+                  unique(as.character(abc$Location)))),
+
+      selectInput("course",
+                  "Course :",
+                  c("All",
+                  unique(as.character(abc$Course)))),
+      
        sliderInput("fees",
-                  h3 ("Range of fees in Ringgit Malaysia (RM) :"),
+                  "Range of fees in Ringgit Malaysia (RM) :",
                    min = 1000,
                    max = 50000,
-                   value = 1000),
-       radioButtons("type",
-                    h3("Type of University : "),
-                    list ("Public","Private"),
-                    " "),
-       selectInput("state", 
-                   h3 ("State :"),
-                   #"Select a state ",
-                   c("Johor", "Melaka","Kuala Lumpur", "Selangor", "Neger Sembilan", "Pahang", "Penang", "Perak", "Kedah", "Perlis", "Kelantan", "Terengganu", "Sabah", "Sarawak")),
-       
-      # selectizeInput("pointer",
-       #               h3("Pointer"),
-        #              c("4.00 - 3.50","3.49 - 3.00","2.99 - 2.50","2.49 - 2.00"),
-         #             " "),
-       selectInput("course", 
-                  h3 ("Course :"),
-                  #"Select a state ",
-                  c("Medic", "Law", "Engineering","Computer Science","Education"),
-                  " "),
-       submitButton(text  = "Submit")
+                   value = c(1000,5000),
+                   width = 4000),
       
+      sliderInput("pointer",
+                  "CGPA :",
+                  min = 2.0,
+                  max = 4.0,
+                  value = 0.5,
+                  step = 0.1)
+      
+      #actionButton("goButton", "Submit")
+    
+  
     
     ),
-    
-   
-
-    
-   
     mainPanel(
-       h3("University"),
-       tableOutput("view"))
+       tableOutput("table")
        )
     )
-  )
+)
+)
+
+
+
+
+
+  
        
 
